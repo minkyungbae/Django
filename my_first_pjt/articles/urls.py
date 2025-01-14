178194a6436f92d6ce2aca66e64ffd427b849ef7
minkyungbae <bmk0703@naver.com>
2025-01-14 16:15:45 +0900

    fix

--- my_first_pjt/articles/urls.py
+++ my_first_pjt/articles/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('hello/', views.hello, name="hello"),
     path('data_throw/', views.data_throw, name="data_throw"),
     path('data_catch/', views.data_catch, name="data_catch"),
+    
+    path('', views.articles, name="articles"),
+    path('create/', views.create, name="create"),
+    path('new/', views.new_article, name="new_article"),
+    
 ]
--- my_first_pjt/articles/views.py
+++ my_first_pjt/articles/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
-
-def hello(request):
-    name = "MinKyung"
-    tags = ["Python", "Django", "GitHub", "html"]
-    personality = ["Down to earth", "Considerate", "Empahty", "Diligent"]
-    context = {
-        "name": name,
-        "tags": tags,
-        "personality": personality
-    }
-    return render(request, "hello.html",context)
 
 def data_throw(request):
     return render(request, "data_throw.html")
@@ -24,4 +14,18 @@ def data_catch(request):
     return render(request, "data_catch.html", context)
 
 
+def articles(request):
+    articles = Article.objects.all().order_by("-created_at")
+    content = {"articles": articles}
+    return render(request, "articles.html", content)
 
+def create(request):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    article = Article(title=title, content=content)
+    article.save()
+    
+    return render(request, "create.html")
+
+def new_article(request):
+    return render(request, "new_article.html")
