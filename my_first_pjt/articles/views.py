f8f99cf81d14c44ae503e48b033bce6dd7428825
minkyungbae <bmk0703@naver.com>
2025-01-14 22:44:24 +0900

    fix add def

--- my_first_pjt/articles/views.py
+++ my_first_pjt/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
@@ -19,13 +19,38 @@ def articles(request):
     content = {"articles": articles}
     return render(request, "articles.html", content)
 
+def article_detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {"article": article}
+    return render(request, "article_detail.html", context)
+
 def create(request):
     title = request.POST.get("title")
     content = request.POST.get("content")
-    article = Article(title=title, content=content)
+    article = Article.objects.create(title=title, content=content)
     article.save()
-    
-    return render(request, "create.html")
+    return redirect('article_detail', article.pk)
 
 def new_article(request):
     return render(request, "new_article.html")
+
+def delete(request, pk):
+    if request.method == "POST":
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return redirect("articles")
+    
+    return redirect('article_detail', pk)
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {"article": article}
+    return render(request, "edit.html", context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get("title")
+    article.content = request.POST.get("content")
+    article.save()
+    return redirect("article_detail", article.pk)
+
