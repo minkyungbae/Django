f0250658d36952a60923da4da0a688174141471a
minkyungbae <bmk0703@naver.com>
2025-01-15 21:47:01 +0900

    Fix urls.py, views.py code

--- my_first_pjt/users/urls.py
+++ my_first_pjt/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from users import views
 
+app_name = "users"
+
 urlpatterns = [
     path('', views.users, name="users"),
     path('profile/<str:username>/', views.profile, name="profile"),
--- my_first_pjt/users/views.py
+++ my_first_pjt/users/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def users(request):
-    return render(request, "users.html")
+    return render(request, "users/users.html")
 
 def profile(request, username):
     context = {"username": username,}
-    return render(request, "profile.html", context)
+    return render(request, "users/profile.html", context)
