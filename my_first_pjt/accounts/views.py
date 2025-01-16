31b2ccd0887fc04b8f5e0729c9ce29a40b9d96fc
minkyungbae <bmk0703@naver.com>
2025-01-16 22:13:15 +0900

    Add new file

--- my_first_pjt/accounts/views.py
+++ my_first_pjt/accounts/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.http import require_POST
 
 
+# 로그인
 def login(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            auth_login(request, form.get_user())
-            return redirect("index")
-    else:
+            # Log the user in
+            auth_login(request, form.get_user())   # 로그인 하기
+            next_url = request.GET.get("next") or "index"
+            return redirect(next_url)
+    
+    else:    
         form = AuthenticationForm()
-    context = {"form": form}
-    return render(request, "accounts/login.html", context)
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
 
-
+# 로그아웃
+@require_POST
+def logout(request):
+    auth_logout(request)    # 로그아웃 하기
+    return redirect('index')
