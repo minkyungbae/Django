fdc5090237c0ebce307b6bcfef1c4cec73706f13
minkyungbae <bmk0703@naver.com>
2025-01-17 16:37:43 +0900

    Fix code, add 'import settings, static', if

--- my_first_pjt/my_first_pjt/urls.py
+++ my_first_pjt/my_first_pjt/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from articles import views
 
 urlpatterns = [
@@ -25,3 +27,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('accounts/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
