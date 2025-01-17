dea16d8d2a9ede21e3820fc51853e83407b9b0bb
minkyungbae <bmk0703@naver.com>
2025-01-17 22:30:07 +0900

    Fix code, add class Article

--- my_first_pjt/articles/models.py
+++ my_first_pjt/articles/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="image/", blank=True)
     
     def __str__(self):
         return self.title
