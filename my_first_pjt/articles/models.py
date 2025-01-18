f2b02d54a1972fb7162096a1e841231414700491
minkyungbae <bmk0703@naver.com>
2025-01-18 17:20:49 +0900

    update

--- my_first_pjt/articles/models.py
+++ my_first_pjt/articles/models.py
@@ -12,7 +12,7 @@ class Article(models.Model):
     
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    content = models.CharField(max_length=255)
+    content = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
