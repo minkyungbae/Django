585285a0f8abfec8ff412c4e592e2aae1941e65a
minkyungbae <bmk0703@naver.com>
2025-01-18 18:02:14 +0900

    Fix class Comment code

--- my_first_pjt/articles/models.py
+++ my_first_pjt/articles/models.py
@@ -11,7 +11,9 @@ class Article(models.Model):
         return self.title
     
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="comments"
+        )
     content = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
