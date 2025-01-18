be081642ecb7f1e7d76eb1e720cf6b4c460a1d5f
minkyungbae <bmk0703@naver.com>
2025-01-18 15:33:32 +0900

    Fix code, add CommentForm

--- my_first_pjt/articles/forms.py
+++ my_first_pjt/articles/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = "__all__"
         exclude = ("created_at", "updated_at")
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = "__all__"
         
