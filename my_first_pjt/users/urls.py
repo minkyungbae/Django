2eaab87c834a022e4fa0baaeba75063338025506
minkyungbae <bmk0703@naver.com>
2025-01-19 16:23:11 +0900

    Fix code, follow

--- my_first_pjt/users/urls.py
+++ my_first_pjt/users/urls.py
@@ -6,4 +6,5 @@ app_name = "users"
 urlpatterns = [
     path('', views.users, name="users"),
     path('profile/<str:username>/', views.profile, name="profile"),
+    path("<int:user_id>/follow/", views.follow, name="follow"),
 ]
