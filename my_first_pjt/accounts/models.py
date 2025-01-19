d715b7b4aca58c57d20d0c6398e17c506cd8983d
minkyungbae <bmk0703@naver.com>
2025-01-19 15:43:37 +0900

    update

--- my_first_pjt/accounts/models.py
+++ my_first_pjt/accounts/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    followings = models.ManyToManyField(
+        "self", related_name="followers", symmetrical=False)
